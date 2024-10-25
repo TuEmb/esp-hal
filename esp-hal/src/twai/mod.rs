36af54033305709cfc449c58bc121788acc905c9
Tu Nguyen <tu.nguyen@ionmobility.com>
2024-10-25 15:54:06 +0700

    add log

--- esp-hal/src/twai/mod.rs
+++ esp-hal/src/twai/mod.rs
@@ -1850,11 +1850,12 @@ mod asynch {
                 // Check if the RX/TX error counter has reached or exceeded.
                 if status.err_st().bit_is_set() {
                     // Read the current TX error counter value
-                    let current_tx_err_cnt = register_block.tx_err_cnt().read().tx_err_cnt().bits();
+                    let tx_err_cnt = register_block.tx_err_cnt().read().tx_err_cnt().bits();
+                    info!("tx error counter is : {}", tx_err_cnt);
                     // Write the incremented TX error count back
                     register_block
                         .tx_err_cnt()
-                        .write(|w| unsafe { w.tx_err_cnt().bits(current_tx_err_cnt + 1) });
+                        .write(|w| unsafe { w.tx_err_cnt().bits(tx_err_cnt + 1) });
                     return Poll::Ready(Err(EspTwaiError::WarningLimit));
                 }
 
